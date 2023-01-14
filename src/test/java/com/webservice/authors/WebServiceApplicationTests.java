5f970307efe774ae1c82184d55e66539b2ac3027
Svyat Danilov <danilov.svat@mail.ru>
2023-01-14 18:24:54 +0300

    Create branch develop

--- src/test/java/com/webservice/authors/WebServiceApplicationTests.java
+++ src/test/java/com/webservice/authors/WebServiceApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class WebServiceApplicationTests {
 
+	//скоро здесь будут тесты....
 	public WebServiceApplicationTests() {
 	}
 
